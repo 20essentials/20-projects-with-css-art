0614600952cdee5f857c6f0921917da488162017
DominicDev <s0l0estudi4r@gmail.com>
2025-10-02 23:02:18 +0200

    v0.0.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    assets: 'static'
+  },
+
+  //Localhost
+  site: 'http://localhost:4321/'
+
+  //Deploy normal project
+  // base: '/20-images-and-svgs-with-krita/',
+  // site: 'https://20essentials.github.io/20-images-and-svgs-with-krita/'
+});
